675cd0849b5ed8e8cd5d5976ab415738db4a9cda
Jonas Singbo <jnax09@gmail.com>
2018-12-16 15:29:51 +0200

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react"
+import ApolloClient from "apollo-boost"
+import { ApolloProvider } from "react-apollo"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import "./App.css"
+import logo from "./logo.png"
+import Launches from "./component/Launches"
+import Launch from "./component/Launch"
+
+const client = new ApolloClient({
+  uri: "/graphql"
+})
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+          <div className="container">
+            <img
+              src={logo}
+              alt="logo"
+              style={{
+                width: 300,
+                margin: "auto",
+                display: "block"
+              }}
+            />{" "}
+            <Route exact path="/" component={Launches} />{" "}
+            <Route exact path="/launch/:flight_number" component={Launch} />{" "}
+          </div>{" "}
+        </Router>{" "}
+      </ApolloProvider>
+    )
+  }
+}
+
+export default App
---
+++ public/precache-manifest.8c04cfd8a131bf7d8b5fbbf7d0802770.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9d7e70fbf0c3f4dd2cbf",
+    "url": "/static/js/main.9d7e70fb.chunk.js"
+  },
+  {
+    "revision": "c76f255625061c0c7545",
+    "url": "/static/js/1.c76f2556.chunk.js"
+  },
+  {
+    "revision": "9d7e70fbf0c3f4dd2cbf",
+    "url": "/static/css/main.b728bf08.chunk.css"
+  },
+  {
+    "revision": "15de72ff5775f518bbae7cd426c789d5",
+    "url": "/index.html"
+  }
+];
